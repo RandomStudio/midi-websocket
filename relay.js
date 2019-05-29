5a6bd111b2836c68a0513f72f361293dd08f9b2c
Stephen Buchanan <stephen.buchanan@random.studio>
2019-05-29 14:04:03 +0200

    log incoming messages

--- relay.js
+++ relay.js
@@ -5,8 +5,15 @@ const portCount = input.getPortCount();
 
 if (portCount === 0) {
     console.error('no MIDI devices found!');
+} else {
+    console.log('found', portCount, 'midi devices');
 }
 
 for (let i = 0; i < portCount; i++) {
     console.log('input #' + i + ': ' + input.getPortName(i));
+
+    input.on('message', (delta, message) => {
+        console.log(message);
+    })
+
 }
